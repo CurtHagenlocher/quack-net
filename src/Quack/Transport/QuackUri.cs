bef0994873338445b1803e1b9d512cc07a544027
Curt Hagenlocher <curt@hagenlocher.org>
2026-05-17 06:18:41 -0700

    Add copyright header to all source files
    Two-line Apache 2.0 attribution at the top of every .cs/.m/.pqm file
    (production, tests, and Power BI connector). Skipped generated obj/bin
    output and non-source files (csproj/resx/md).

--- src/Quack/Transport/QuackUri.cs
+++ src/Quack/Transport/QuackUri.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Curt Hagenlocher. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
 namespace Quack.Transport;
 
 // Parser/builder for `quack:host[:port]` URIs. Mirrors duckdb_quack::QuackUri.
--- test/Quack.IntegrationTests/SessionExpiredTests.cs
+++ test/Quack.IntegrationTests/SessionExpiredTests.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Curt Hagenlocher. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
 using Quack;
 using Quack.Data;
 using Quack.Protocol;
